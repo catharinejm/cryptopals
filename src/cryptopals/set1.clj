c93a71e01de00ebcea88f5447843e89049c1ce87
Jon Distad <jon.distad@gmail.com>
2014-11-20 11:59:02 -0500

    add per-challenge sanity checks

--- src/cryptopals/set1.clj
+++ src/cryptopals/set1.clj
@@ -228,7 +228,7 @@
   (let [hex (sanitize-hex hex)]
     (reduce best-score
             (for [n (range 256)]
-              (score-map hex n)))))
+              (score-map-hex hex n)))))
 
 (defn find-encoded-hex
   [file]
@@ -238,8 +238,8 @@
 
 (defn repeating-key-xor
   [input key]
-  (apply str (map #(char (xor (int %1) (int %2)))
-                  input (cycle key))))
+  (map #(xor (int %1) (int %2))
+       input (cycle key)))
 
 (defn hamming-distance
   [buf1 buf2]
@@ -282,4 +282,52 @@
   [file]
   (let [bytes (sequence (comp cat base64->bytes*) (line-seq (io/reader file)))
         key (guess-key bytes)]
-    (repeating-key-xor bytes key)))
+    (apply str (map char (repeating-key-xor bytes key)))))
+
+(defn challenge1
+  []
+  (let [in "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+        expected "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+        result (hex->base64 in)]
+    (assert (= expected result))
+    result))
+
+(defn challenge2
+  []
+  (let [in1 "1c0111001f010100061a024b53535009181c"
+        in2 "686974207468652062756c6c277320657965"
+        expected "746865206b696420646f6e277420706c6179"
+        result (xor-hex in1 in2)]
+    (assert (= expected result))
+    result))
+
+(defn challenge3
+  []
+  (let [in "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+        expected "Cooking MC's like a pound of bacon"
+        result (:result (pick-string in))]
+    (assert (= expected result))
+    result))
+
+(defn challenge4
+  []
+  (let [expected {:hex "7b5a4215415d544115415d5015455447414c155c46155f4058455c5b523f"
+                  :result "Now that the party is jumping\n"
+                  :cipher \5}
+        result (find-encoded-hex "resources/4.txt")]
+    (assert (= expected (select-keys result (keys expected))))
+    result))
+
+(defn challenge5
+  []
+  (let [in "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+        key "ICE"
+        expected (str "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"
+                      "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
+        result (apply str (sequence bytes->hex* (repeating-key-xor in key)))]
+    (assert (= expected result))
+    result))
+
+(defn challenge6
+  []
+  (println (break-repeating-xor "resources/6.txt")))
