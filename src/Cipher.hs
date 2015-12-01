202626970d9df2310d6ddd7c14ce5d4f46a3f4f9
Jon Distad <jon.distad@gmail.com>
2015-12-01 00:18:49 -0800

    Resolve padding weirdness

--- src/Cipher.hs
+++ src/Cipher.hs
@@ -1,7 +1,7 @@
 module Cipher where
 
 import           BufferOps
-import           Control.Monad.RWS
+import           Control.Monad.State
 import qualified Crypto.Cipher.AES as AES
 import           Data.Binary.Put
 import           Data.ByteString.Lazy (ByteString)
@@ -14,23 +14,16 @@ blockLength :: Int64
 blockLength = 16
 
 
-initAES :: BS.ByteString -> AESKey
+initAES :: ByteString -> AESKey
 initAES = AES.initAES . BS.toStrict
 
 
+decryptBlock :: AESKey -> ByteString -> ByteString
+decryptBlock key = BS.fromStrict . AES.decryptECB key . BS.toStrict
+
+
 decryptECB :: AESKey -> ByteString -> ByteString
-decryptECB key = BS.fromStrict . AES.decryptECB key . BS.toStrict
-
-
-blockMap :: Int64 -> (ByteString -> ByteString) -> ByteString -> ByteString
-blockMap bsize f buf = runPut $ perform $ groupedBuffer blockLength buf
-  where
-    perform :: [ByteString] -> Put
-    perform [] = putByteString (f $ pkcs7Pad BS.empty)
-    perform [b] | BS.length b < blockLength = putByteString (f $ pkcs7Pad b)
-    perform (b:bs) = do
-      putByteString $ f b
-      perform bs
+decryptECB key = pkcs7Unpad blockLength . decryptBlock key
 
 
 encryptBlock :: AESKey -> ByteString -> ByteString
@@ -38,7 +31,7 @@ encryptBlock key = BS.fromStrict . AES.encryptECB key . BS.toStrict
 
 
 encryptECB :: AESKey -> ByteString -> ByteString
-encryptECB key = blockMap blockLength encryptBlock
+encryptECB key = encryptBlock key . pkcs7Pad blockLength
 
 
 pkcs7Pad :: Int64 -> ByteString -> ByteString
@@ -47,23 +40,37 @@ pkcs7Pad len buf = padded
     bufLen = BS.length buf
     padLen = len - bufLen `mod` len
     pad = BS.take padLen $ BS.repeat $ fromIntegral padLen
-    padded = if bufLen < len then BS.concat [buf, pad] else buf
+    padded = BS.concat [buf, pad]
+
+
+pkcs7Unpad :: Int64 -> ByteString -> ByteString
+pkcs7Unpad len buf = BS.take (len - padLen) buf
+  where
+    padLen = fromIntegral $ BS.last buf
 
 
 encryptCBC :: AESKey -> ByteString -> ByteString -> ByteString
 encryptCBC key iv buf = BS.concat $ encrypt iv buf
   where
-    encrypt l b | BS.null b = pkcs7Pad
-    encrypt l b = let block = pkcs7Pad blockLength $ BS.take blockLength b
-                      enc = encryptChunk l block
-                  in enc : encrypt enc (BS.drop blockLength b)
-    encryptChunk last cur = encryptECB key $ xorBuffers last cur
+    pad = pkcs7Pad blockLength
+    encrypt l b = let cur = BS.take blockLength b
+                  in if BS.length cur < blockLength
+                     then [encryptChunk l $ pad cur]
+                     else let enc = encryptChunk l $ cur
+                          in enc : encrypt enc (BS.drop blockLength b)
+    encryptChunk last = encryptBlock key . xorBuffers last
 
 
 decryptCBC :: AESKey -> ByteString -> ByteString -> ByteString
 decryptCBC key iv buf = BS.concat $ decrypt iv buf
   where
-    decrypt l b | BS.null b = []
-    decrypt l b = let chunk = BS.take blockLength b
-                  in decryptChunk l chunk : decrypt chunk (BS.drop blockLength b)
-    decryptChunk l b = xorBuffers l $ decryptECB key b
+    validate b = if BS.length b < blockLength
+                 then error "Premature end of encrypted block"
+                 else b
+    decrypt l b = let cur = validate $ BS.take blockLength b
+                      dec = decryptChunk l cur
+                      next = BS.drop blockLength b
+                  in if BS.null next
+                     then [pkcs7Unpad blockLength dec]
+                     else dec : decrypt cur (BS.drop blockLength b)
+    decryptChunk l = xorBuffers l . decryptBlock key
